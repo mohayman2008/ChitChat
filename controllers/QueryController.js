ea34634be2150bae83e271df0acb535fddd7a2ba
Mohamed Ayman <mohayman2007@gmail.com>
2024-07-02 18:02:38 +0000

    Adding server side getUsers handler

---
+++ controllers/QueryController.js
@@ -0,0 +1,23 @@
+import { verifyKey } from '../controllers/AuthController';
+import { User, UserStatus, Conversation, Message } from '../models/models',
+
+export default class QueryController {
+  
+  static async getUsers(data) {
+    if (!data[key]) {
+      return { error: 'key-token is missing' };
+    }
+    const user = await verifyKey(data[key]);
+    if (!user) {
+      return { error: 'Authentication failed' };
+    }
+
+    return {
+      status: 'OK',
+      users: User.findAll(
+        {
+          attributes: ['id', 'name', 'picture', 'status'],
+        })
+    };
+  }
+}
